806deefb0d23520ce8aa7d1cb7be6530cd37569d
Bukunmi-sketch <Olarindebukunmi@gmail.com>
2023-01-09 07:09:26 +0100

    Update main.dart
    ---

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,25 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  var questions = ['what\'s killed osama bin laden'];
+
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('text hello'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Bucuzzi'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Text('fuck you all'),
+              RaisedButton(child: Text('answer 1')),
+              RaisedButton(child: Text('answer 1')),
+              RaisedButton(child: Text('answer 1')),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
