43408fee9e357f5cab11966866647e0cfd38e454
Bukunmi-sketch <Olarindebukunmi@gmail.com>
2023-04-07 12:26:46 +0100

    added assets
    added images to it

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,6 +5,31 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        backgroundColor: Colors.grey[300],
+        body: Column(
+          children: [
+            //LOGO
+            Icon(
+              Icons.lock,
+              size: 100,
+            )
+            // Welcome back we missed you
+
+            //username textfield
+
+            //passsword textfield
+
+            //forgot password question
+
+            //signin button
+
+            //or continue with
+
+            //google + apples signin
+
+            //not a member ? register now
+          ],
+        ));
   }
 }
