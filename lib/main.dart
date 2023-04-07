237e643e571b62931126748ad5c39327ae489b21
Bukunmi-sketch <Olarindebukunmi@gmail.com>
2023-04-07 11:37:15 +0100

    re-starting
    re-starting

--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,18 @@
 import 'package:flutter/material.dart';
+import 'pages/login_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  var questions = ['what\'s killed osama bin laden'];
+  const MyApp({super.key});
 
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Xermux'),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              Container(
-                  alignment: Alignment.center,
-                  padding: const EdgeInsets.all(10),
-                  child: const Text('Login Now',
-                      style: TextStyle(
-                          color: Colors.blue,
-                          fontWeight: FontWeight.w500,
-                          fontSize: 30)),
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                          color: Colors.transparent,
-                          style: BorderStyle.solid,
-                          width: 2))),
-              Container(
-                decoration:
-                    BoxDecoration(border: Border.all(color: Colors.green)),
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(20),
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Username',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-              ),
-              Container(
-                decoration:
-                    BoxDecoration(border: Border.all(color: Colors.green)),
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(20),
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-              ),
-              TextButton(
-                  onPressed: () {
-                    print('forgot');
-                  },
-                  child: const Text('forgot password')),
-              ElevatedButton(
-                child: const Text(
-                  'Login',
-                  style: TextStyle(fontSize: 13.0, fontFamily: "Brand Bold"),
-                ),
-                onPressed: () {
-                  print('login clicked');
-                },
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text('Does not have an account?'),
-                  TextButton(
-                      onPressed: () {
-                        print("create new account");
-                      },
-                      child: const Text('create new account')),
-                ],
-              )
-            ],
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
     );
   }
 }
